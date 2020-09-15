fcbede647b465ac40242f420200765e621b70d6d
Rafael Campos <rafaelccamposdev@gmail.com>
2020-09-15 00:36:18 -0300

    Projeto criado.

---
+++ backend/src/main/java/com/rafaelcampos/rdpesquisa/RdpesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelcampos.rdpesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdpesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdpesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rafaelcampos/rdpesquisa/RdpesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelcampos.rdpesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdpesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
