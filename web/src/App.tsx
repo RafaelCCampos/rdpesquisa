5f897dc5e6ee773a6057afa2f74ab486056ea8f4
Rafael Campos <rafaelccamposdev@gmail.com>
2020-09-20 16:15:39 -0300

    Header e Pagina inicial.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Home from './pages/Home';
+
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Header/>
+      <Home/>
+    </div>
   );
 }
 
