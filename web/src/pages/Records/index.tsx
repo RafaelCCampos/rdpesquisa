995e9a4501111679ff9aedf88d8226206b233823
Rafael Campos <rafaelccamposdev@gmail.com>
2020-09-22 00:04:23 -0300

    Conexão com backend usando axios, tabela de records populada, formatando datas com moment.

--- web/src/pages/Records/index.tsx
+++ web/src/pages/Records/index.tsx
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+
+import {RecordsResponse} from './types';
+import {formatDate} from './helpers';
+
 import './styles.css';
 
-const Records = () => (
-    <h1>Records</h1>
-); 
+const Records = () => {
+    const [ recordsResponse, setRecordsResponse ] = useState<RecordsResponse>();
+    
+    useEffect(() => {
+        api.get('records')
+        .then( response => setRecordsResponse(response.data));
+    },[]);
+
+    return  (
+        <div className="page-container">
+            <table className="records-table" cellPadding="0" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>NOME</th>
+                        <th>IDADE</th>
+                        <th>PLATAFORMA</th>
+                        <th>GÊNERO</th>
+                        <th>TÍTULO DO GAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recordsResponse?.content.map(record => (
+                        <tr key={record.id}>
+                            <td>{formatDate(record.moment)}</td>
+                            <td>{record.name}</td>
+                            <td>{record.age}</td>
+                            <td className="text-secondary">{record.gamePlatform}</td>
+                            <td>{record.genreName}</td>
+                            <td className="text-primary">{record.gameTitle}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    ); 
+}
 
 export default Records;
+
+
+
+
